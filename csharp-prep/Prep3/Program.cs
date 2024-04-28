ed71efb085c1cb21bf99fea1874ccb62d582cd7e
reyaremnta <reyarmenta1@hotmail.com>
2024-04-27 19:07:21 -0600

    Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+         Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+        //int number = 6;
+
+        int guess = -1;
+
+        while (guess != number)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess > number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }       
     }
 }
