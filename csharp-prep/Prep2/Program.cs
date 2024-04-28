7fc42dfca3e9f729dc453b3644e67445bbc490a9
reyaremnta <reyarmenta1@hotmail.com>
2024-04-27 18:56:43 -0600

    Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,49 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+          Console.Write("What is your grade percentage? ");
+         string grade_percentage = Console.ReadLine();
+         int number = int.Parse(grade_percentage);
+
+         string letter = "";
+
+         if (number >= 90)
+         {
+            //Console.WriteLine("A");
+            letter = "A";
+         }
+         else if (number >= 80)
+         {
+            //Console.WriteLine("B");
+            letter = "B";
+         }
+         else if (number >= 70)
+         {
+            //Console.WriteLine("C");
+            letter = "C";
+         }
+         else if (number >= 60)
+         {
+            //Console.WriteLine("D");
+            letter = "D";
+         }
+         else if (number < 60)
+         {
+            //Console.WriteLine("F");
+            letter = "F";
+         }
+
+         Console.WriteLine($"Your grade is: {letter}");
+
+
+        //Determine if the student passed or not
+         if (number >= 70)
+         {
+            Console.WriteLine("Congratulations, you passed!");
+         }
+         else 
+         {
+            Console.WriteLine("You can worke harder and able to pass the class :) ");
+         }
     }
 }
