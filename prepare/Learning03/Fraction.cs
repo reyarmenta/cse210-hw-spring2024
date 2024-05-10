7fc2b35bb4cfc23494b068a871ae01034431dab6
reyaremnta <reyarmenta1@hotmail.com>
2024-05-09 20:49:15 -0600

    fraction

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,39 @@
+using System.Diagnostics;
+
+public class Fraction
+{
+    private int _top;
+    private int _bottom;
+
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+
+    }
+
+    public Fraction(int num)
+    {
+        _top = num;
+        _bottom = 1;
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+    
+    public string GetFractionString()
+    {
+        string text = $"{_top}/{_bottom}";
+        return text;
+    }
+
+    public double GetDecimalValue()
+    {
+        return (double)_top / (double)_bottom;
+    }
+}
+
+    
